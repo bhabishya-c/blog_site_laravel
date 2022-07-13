3a40c12b7c43ef3051e470f69d4925803ceb55b7
Bhabishya Chaudhary <bhabishya.chaudhary@introcept.co>
2022-07-13 12:42:59 +0545

    Updated Postcontroller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Post;
-use App\Models\Comment;
 use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
