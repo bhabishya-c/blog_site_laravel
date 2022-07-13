9b365ece7e626c6a3e4646ffe701b47463639db9
Bhabishya Chaudhary <bhabishya.chaudhary@introcept.co>
2022-07-13 12:39:07 +0545

    Updated adminhome and userhome.blade.php page

--- resources/views/userhome.blade.php
+++ resources/views/userhome.blade.php
@@ -71,7 +71,7 @@
                             <input type="submit" class="btn btn-info btn-md" value="Delete" style="border-radius:50px;">
                             </form>
                             @endif
-                            <form action="blogcontent" method="get" style="display:inline;">
+                            <form action="" method="get" style="display:inline;">
                             @csrf
                             <input type="hidden" name="id" value="{{$u->id}}">
                             <input type="submit" class="btn btn-info btn-md" value="Readmore" style="border-radius:50px;"><br>
